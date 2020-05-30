0e6f1d94005b1e0ebb0250904c91635a1647ed8f
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-05-29 18:45:57 -0700

    -x overflow fixed

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ function goHome()
 {
   window.location = '/';   
 }
+
+let docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
