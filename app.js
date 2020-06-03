86173aa0d910818d29213377f40ebadf464208f6
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-06-02 18:38:08 -0700

    updating readme

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+let image = document.querySelector('about__shape');
+window.addEventListener('scroll', function (event) {
+	if (isInViewport(image)) {
+		image.innerHTML = '<img src="' + image.getAttribute('about__shape') + '">';
+	}
+}, false);
+
+let isInViewport = function (el) {
+  let bounding = el.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
 function goHome()
 {
   window.location = '/';   
 }
-
-let docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
-  }
-);
