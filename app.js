949d9357bb9b959df97ab74aef6da5e9e2fd7e57
Marine Mekhakyan <marinemekhakyan@gmail.com>
2020-06-14 22:41:19 -0700

    removing extra js code

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-let image = document.querySelector('about__shape');
-window.addEventListener('scroll', function (event) {
-	if (isInViewport(image)) {
-		image.innerHTML = '<img src="' + image.getAttribute('about__shape') + '">';
-	}
-}, false);
-
-let isInViewport = function (el) {
-  let bounding = el.getBoundingClientRect();
-  return (
-      bounding.top >= 0 &&
-      bounding.left >= 0 &&
-      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-};
-
 function goHome()
 {
   window.location = '/';   
